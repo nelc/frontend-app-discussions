43d658f7409e50672587f06acdc69759f7f82440
Brayan Cerón <86393372+bra-i-am@users.noreply.github.com>
2024-07-29 16:11:57 -0500

    perf: update redwood branch with upstream  (#7)
    * feat: import `FooterSlot` from `frontend-slot-footer` package
    
    * fix: block overflow when editing comment (#710)
    
    * fix: fixed author liking its own post (#729)
    
    * perf: update redwood branch with upstream
    
    * refactor: generate package-lock with conflicts solved
    
    * fix: tests failed because axios and react-markdown lib errors
    
    ---------
    
    Co-authored-by: Brian Smith <bsmith@axim.org>
    Co-authored-by: Ihor Romaniuk <ihor.romaniuk@raccoongang.com>
    Co-authored-by: ayesha waris <73840786+ayesha-waris@users.noreply.github.com>

--- jest.config.js
+++ jest.config.js
@@ -11,4 +11,12 @@ module.exports = createConfig('jest', {
     'src/setupTest.jsx',
     'src/i18n',
   ],
+  // see https://github.com/axios/axios/issues/5026
+  moduleNameMapper: {
+    "^axios$": "axios/dist/axios.js",
+    // See https://stackoverflow.com/questions/72382316/jest-encountered-an-unexpected-token-react-markdown
+    'react-markdown': '<rootDir>/node_modules/react-markdown/react-markdown.min.js',
+  },
+  testTimeout: 30000,
+  testEnvironment: 'jsdom'
 });
